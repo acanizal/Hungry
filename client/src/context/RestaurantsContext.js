739d2b50ed97aacb777885e5404cc823e0ed5744
acanizal <alex.canizal.d@gmail.com>
2024-01-31 22:19:44 -0600

    Add restaurants to the back and front end

--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -5,8 +5,13 @@ export const RestaurantsContext = createContext();
 export const RestaurantContextProvider = (props) => {
     const [restaurants, setRestaurants] = useState([])
 
+    const addRestaurants = (restaurant) => {
+        //Adds current restaurants to new array
+        setRestaurants([...restaurants, restaurant]);
+    }
+
     return (
-        <RestaurantsContext.Provider value={{restaurants, setRestaurants}}>
+        <RestaurantsContext.Provider value={{restaurants, setRestaurants, addRestaurants}}>
             {props.children}
         </RestaurantsContext.Provider>
     )
